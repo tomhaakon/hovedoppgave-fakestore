ac86dbd11dc614b89ba96cab0fdbb17ed511221f
tomhaakon <tom.fjeldheim@gmail.com>
2023-08-13 23:07:08 +0200

    småfix

--- src/views/ShoppingCartView.vue
+++ src/views/ShoppingCartView.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="mx-10">
-    <div class="flex h-10">
-      <div class="w-1/4"></div>
-      <div class="w-2/4">
-        <p class="text-center font-bold text-xl p-1">Shopping cart</p>
-      </div>
-      <div class="w-1/4"></div>
+  <div class="m-5">
+    <div class="flex h-10 w-full mb-5">
+      <p class="font-bold text-xl p-1">Shopping cart</p>
     </div>
     <!-- sd -->
     <div class="">
       <div class="flex pb-2">
         <div class="w-5 flex justify-center"></div>
         <div class="w-full px-5"><p class="font-bold text-sm">Title</p></div>
-        <div class="w-40 text-left">
+        <div class="w-28 text-left">
           <p class="font-bold text-sm">Price</p>
         </div>
         <div class="w-5"></div>
@@ -21,24 +17,32 @@
         v-for="item in groupedCart"
         :key="item.id"
         v-if="groupedCart"
-        class="flex shadow-md"
+        class="flex shadow-sm"
       >
         <div class="w-5 flex justify-center">{{ item.count }}x</div>
         <div class="w-full px-5">{{ item.title }}</div>
-        <div class="w-40 text-left">
-          {{ Math.round(item.price * 10 * item.count) }}
+        <div class="w-28 text-left">
+          {{ Math.round(item.price * 10 * item.count) }},-
         </div>
-        <div class="w-5 text-right">
+        <div class="w-10">
           <button class="" @click="removeFromCart(item.id)">x</button>
         </div>
       </div>
     </div>
-    <div class="flex w-full pt-5">
+    <div class="w-full flex pt-3">
       <div class="w-full">
-        <button class="" @click="clearCart">
-          <p class="">Clear Cart</p>
+        <button
+          class=""
+          @click="clearCart"
+          v-if="shoppingCartStore.cartItemCount > 0"
+        >
+          <p class="text-gray-500">clear cart</p>
         </button>
       </div>
+      <div class="w-80 text-right">Total: {{ totalPrice }},-</div>
+    </div>
+    <div class="flex w-full">
+      <div class="w-full"></div>
       <div class="flex justify-end">
         <button class="btn btn-primary mt-5">To payment</button>
       </div>
@@ -67,6 +71,11 @@ const groupedCart = computed(() => {
       count: counts[id],
     };
   });
+});
+const totalPrice = computed(() => {
+  return groupedCart.value.reduce((sum, item) => {
+    return sum + Math.round(item.price * 10 * item.count);
+  }, 0);
 });
 
 // consts
