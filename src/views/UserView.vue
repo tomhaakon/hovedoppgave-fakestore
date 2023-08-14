a8c2b7f964822d8d504f1e50ce12a16cc1fe0ee2
tomhaakon <tom.fjeldheim@gmail.com>
2023-08-15 01:13:16 +0200

    mobiltest

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,29 +1,31 @@
 <template>
-  <div v-if="viewParam === 'login'">
-    <div class="">
-      <p class="font-bold text-xl p-1">Login</p>
+  <div class="px-5">
+    <div v-if="viewParam === 'login'">
+      <div class="">
+        <p class="font-bold text-xl p-1">Login</p>
+      </div>
+      <Login />
+      <div class="w-full text-center px-14">
+        <p class="py-5">or</p>
+        <RouterLink to="/register">
+          <p>click here to register new account</p>
+        </RouterLink>
+      </div>
     </div>
-    <Login />
-    <div class="w-full text-center px-14">
-      <p class="py-5">or</p>
-      <RouterLink to="/register">
-        <p>click here to register new account</p>
-      </RouterLink>
-    </div>
-  </div>
-  <div v-if="viewParam === 'register'">
-    <div class="">
-      <p class="font-bold text-xl p-1">Register</p>
-      <p class="p-5 text-red-600">
-        This register form is for training purposes only so passwords are not
-        encrypted and are stored in you browsers localStorage.
-      </p>
-    </div>
-    <Register />
-    <div class="w-full text-center py-5 px-14">
-      <RouterLink to="/login">
-        <p>return to log in section</p>
-      </RouterLink>
+    <div v-if="viewParam === 'register'">
+      <div class="">
+        <p class="font-bold text-xl p-1">Register</p>
+        <p class="p-5 text-red-600">
+          This register form is for training purposes only so passwords are not
+          encrypted and are stored in you browsers localStorage.
+        </p>
+      </div>
+      <Register />
+      <div class="w-full text-center py-5 px-14">
+        <RouterLink to="/login">
+          <p>return to log in section</p>
+        </RouterLink>
+      </div>
     </div>
   </div>
 </template>
