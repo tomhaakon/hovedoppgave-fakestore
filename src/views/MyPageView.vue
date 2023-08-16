efee08549e74e8d99098af3fd53d1dd7945efc01
tomhaakon <tom.fjeldheim@gmail.com>
2023-08-16 14:56:30 +0200

    mobiletest

--- src/views/MyPageView.vue
+++ src/views/MyPageView.vue
@@ -4,18 +4,25 @@
       <p class="font-bold text-xl p-1 pb-5">MyPage</p>
     </div>
     <div class="pb-5">
-      Hello <span class="font-bold">{{ loggedInUser }}</span
-      ><span>, this is your purchase history</span>
+      Hello <span class="font-bold">{{ loggedInUser }}</span>
+      <p class="pt-5 font-bold">Purchase History</p>
     </div>
-    <div class="bg-cyan-200">
+    <div class="">
       <div class="space-y-5">
-        <div v-for="(purchase, index) in purchaseHistory" :key="index">
-          <div class="w-full"></div>
-          <div class="flex">
-            <div class="w-20">x</div>
-            <div class="w-full">{{ purchase.date }}</div>
+        <div
+          v-for="(purchase, index) in purchaseHistory"
+          :key="index"
+          v-if="purchaseHistory.length > 0"
+        >
+          <div></div>
+          <div class="w-full bg-gray-200 flex">
+            <div class="pr-2 w-20">#{{ index }}</div>
+            <div class="w-full">{{ formatDate(purchase.date) }}</div>
           </div>
-          <div class="bg-red-500">
+          <div class="flex">
+            <div class="w-full"></div>
+          </div>
+          <div class="">
             <div
               v-for="(item, itemIndex) in purchase.items"
               :key="itemIndex"
@@ -25,7 +32,16 @@
               <div class="w-full">{{ item.title }}</div>
             </div>
           </div>
+          <div class="flex pt-2">
+            <div class="w-20">total</div>
+            <div class="w-full pb-5">
+              <p class="font-bold">
+                {{ calculateTotalPrice(purchase.items) }}
+              </p>
+            </div>
+          </div>
         </div>
+        <div v-else>You havent purchased anything yet.</div>
       </div>
     </div>
   </div>
@@ -34,11 +50,33 @@
 import { computed, ref } from "vue";
 import { useUserStore } from "@/stores/UserStore";
 
-const totalPrice = ref([]);
-
 const UserStore = useUserStore();
 const loggedInUser = computed(() => UserStore.sessionAuth);
 const purchaseHistory = computed(() => UserStore.user?.purchaseHistory || []);
+
+console.log(purchaseHistory.value.length);
+
+const calculateTotalPrice = (items) => {
+  return items.reduce((total, item) => total + item.price * 10, 0);
+};
+
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  return date
+    .toLocaleString("en-GB", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      timeZone: "Europe/Oslo", // or any other European time zone
+    })
+    .replace(",", " at");
+};
+
+// Usage example:
+const formattedDate = formatDate("2023-08-16T12:00:52.753Z");
+console.log(formattedDate); // Output might be "August 16, 2023, 12:00:52 PM"
 
 //for ((purchase, index) in purchaseHistory) {
 // //console.log(index)
